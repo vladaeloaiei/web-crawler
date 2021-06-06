2c054d6ff92c8b750827660a15f8a3671309539d
Vlad Aeloaiei <vlad.aeloaiei@gmail.com>
2021-06-06 15:48:28 +0300

    Fix resource filtering

--- domain-explorer/src/main/java/com/aeloaiei/dissertation/domain/explorer/http/HTTPResourceFilter.java
+++ domain-explorer/src/main/java/com/aeloaiei/dissertation/domain/explorer/http/HTTPResourceFilter.java
@@ -6,24 +6,20 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static java.util.Arrays.asList;
 import static java.util.Objects.nonNull;
 
 @Component
 public class HTTPResourceFilter {
     private static final Logger LOGGER = LogManager.getLogger(HTTPResourceFilter.class);
 
-    private static final Set<String> DENIED_CONTENT_TYPE = new HashSet<>(asList("audio", "font/", "image", "video", "model"));
+    private static final String ALLOWED_CONTENT_TYPE_PREFIX = "text/";
     private static final int CONTENT_TYPE_PREFIX_SIZE = 5;
 
     public boolean filter(Response response) {
         String contentTypeHeader = response.header(HttpHeaders.CONTENT_TYPE);
 
         if (nonNull(contentTypeHeader)) {
-            return DENIED_CONTENT_TYPE.contains(contentTypeHeader.substring(CONTENT_TYPE_PREFIX_SIZE));
+            return !ALLOWED_CONTENT_TYPE_PREFIX.equals(contentTypeHeader.substring(0, CONTENT_TYPE_PREFIX_SIZE));
         } else {
             return false;
         }
